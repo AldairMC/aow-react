f4feb5cbc580f4ebaeaa23e519b2ca0c63e8b265
AldairMC <aldairmc2023@gmail.com>
2019-06-02 08:55:25 -0500

    components Header finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/Header';
 
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Header 
+          titulo = "App of weather"
+        />
 
-
-function App() {
-  return (
-    <h1>Hola</h1>
-  );
-}
+      </div>
+    );
+  }
+} 
 
 export default App;
